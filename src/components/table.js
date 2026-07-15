16b7271f173cc455267dd272c76da6e973920821
Irina Kondratenko <iiiii.irina.irina@gmail.com>
2026-07-15 15:11:09 +0500

    change smart-table

--- src/components/table.js
+++ src/components/table.js
@@ -11,7 +11,6 @@ export function initTable(settings, onAction) {
   const { tableTemplate, rowTemplate, before, after } = settings;
   const root = cloneTemplate(tableTemplate);
 
-  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
   before.reverse().forEach((subName) => {
     root[subName] = cloneTemplate(subName);
     root.container.prepend(root[subName].container);
@@ -20,7 +19,6 @@ export function initTable(settings, onAction) {
     root[subName] = cloneTemplate(subName);
     root.container.append(root[subName].container);
   });
-  // @todo: #1.3 —  обработать события и вызвать onAction()
   root.container.addEventListener("change", () => {
     onAction();
   });
@@ -34,7 +32,6 @@ export function initTable(settings, onAction) {
     onAction(e.submitter);
   });
   const render = (data) => {
-    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
     const nextRows = data.map((item) => {
       const row = cloneTemplate(rowTemplate);
       Object.keys(item).forEach((key) => {
